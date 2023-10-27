ed50685ff4d22c8eeabc111da41c91d62633852f
MatiasBurecovics <111394291+MatiasBurecovics@users.noreply.github.com>
2023-10-27 11:49:46 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,41 +1,50 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import WeatherComponent from './WeatherComponent';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, ImageBackground } from 'react-native';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 import BackgroundImage from './BackgroundImage';
 
 export default function App() {
+  const [showBackgroundImage, setShowBackgroundImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const saveSelectedImage = async (imageUri) => {
+    try {
+      await AsyncStorage.setItem('selectedImage', imageUri);
+    } catch (error) {
+      console.error('Error saving selected image: ', error);
+    }
+  }
+
+  const getSelectedImage = async () => {
+    try {
+      const imageUri = await AsyncStorage.getItem('selectedImage');
+      return imageUri;
+    } catch (error) {
+      console.error('Error retrieving selected image: ', error);
+    }
+  }
+
+  const handleSelectBackgroundImage = async () => {
+    const imageUri = await getSelectedImage();
+    setSelectedImage(imageUri);
+  };
+
+  useEffect(() => {
+    handleSelectBackgroundImage();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <BackgroundImage />
-      <WeatherComponent />
+    <View style={{ flex: 1 }}>
+      {selectedImage ? (
+        <ImageBackground source={{ uri: selectedImage }} style={{ flex: 1 }}>
+          <BackgroundImage />
+        </ImageBackground>
+      ) : (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Press the button to select a background image</Text>
+          <Button title="Select Background Image" onPress={handleSelectBackgroundImage} />
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  toggleButton: {
-    backgroundColor: '#007AFF',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-});
