8e7b7907857204f4b5bc80e46c086e7e6fa5da6f
MatiasBurecovics <111394291+MatiasBurecovics@users.noreply.github.com>
2023-11-08 09:06:20 -0300

    Add files via upload

--- BackgroundImage.js
+++ BackgroundImage.js
@@ -1,69 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, ImageBackground } from 'react-native';
-import { Camera } from 'expo-camera';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, SafeAreaView, Button, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import AsyncStorage from '@react-native-async-storage/async-storage'; // Importa AsyncStorage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'expo-status-bar';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function BackgroundImage() {
-  const [startCamera, setStartCamera] = useState(false);
-  const [previewVisible, setPreviewVisible] = useState(false);
-  const [capturedImage, setCapturedImage] = useState(null);
-  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
-  const [flashMode, setFlashMode] = useState('off');
-  const [backgroundImage, setBackgroundImage] = useState(null);
-
-  const cameraRef = useRef(null);
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  const [photo, setPhoto] = useState();
+  const [isCameraOpen, setIsCameraOpen] = useState();
 
   useEffect(() => {
-    AsyncStorage.getItem('backgroundImage')
-      .then((value) => {
-        if (value) {
-          setBackgroundImage(value);
-        }
-      });
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
   }, []);
 
-  const sCamera = async () => {
-    const { status } = await Camera.requestPermissionsAsync();
-    console.log(status);
-    if (status === 'granted') {
-      setStartCamera(true);
-    } else {
-      Alert.alert('Access denied');
-    }
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>
+  } else if (!hasCameraPermission) {
+    return <Text>Permission for camera not granted. Please change this in settings.</Text>
   }
 
-  const takePicture = async () => {
-    if (cameraRef.current) {
-      const photo = await cameraRef.current.takePictureAsync();
-      console.log(photo);
-      setPreviewVisible(true);
-      setCapturedImage(photo);
-    }
-  }
+  const takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false
+    };
 
-  const retakePicture = () => {
-    setCapturedImage(null);
-    setPreviewVisible(false);
-  }
-
-  const handleFlashMode = () => {
-    if (flashMode === 'on') {
-      setFlashMode('off');
-    } else if (flashMode === 'off') {
-      setFlashMode('on');
-    } else {
-      setFlashMode('auto');
-    }
-  }
-
-  const switchCamera = () => {
-    setCameraType(prevCameraType =>
-      prevCameraType === Camera.Constants.Type.back
-        ? Camera.Constants.Type.front
-        : Camera.Constants.Type.back
-    );
-  }
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+    setIsCameraOpen(false);
+  };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -72,66 +47,46 @@ export default function BackgroundImage() {
       aspect: [4, 3],
       quality: 1,
     });
-  
-    if (!result.cancelled && result.uri) {
-      await AsyncStorage.setItem('backgroundImage', result.uri);
-      setBackgroundImage(result.uri);
-    }
   };
-  
+
+  if (photo) {
+    let savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
+        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      {startCamera ? (
-        <View style={{ flex: 1, width: '100%' }}>
-          {previewVisible && capturedImage ? (
-            <CameraPreview photo={capturedImage} retakePicture={retakePicture} />
-          ) : (
-            <Camera
-              type={cameraType}
-              flashMode={flashMode}
-              style={{ flex: 1 }}
-              ref={cameraRef}
-            >
-              <View style={{ flex: 1, width: '100%', backgroundColor: 'transparent', flexDirection: 'row' }}>
-                <View style={{ position: 'absolute', left: '5%', top: '10%', flexDirection: 'column', justifyContent: 'space-between' }}>
-                  <TouchableOpacity
-                    onPress={handleFlashMode}
-                    style={{ backgroundColor: flashMode === 'off' ? '#000' : '#fff', borderRadius: 25, height: 25, width: 25 }}
-                  >
-                    <Text style={{ fontSize: 20 }}>⚡️</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={switchCamera}
-                    style={{ marginTop: 20, borderRadius: 25, height: 25, width: 25 }}
-                  >
-                    <Text style={{ fontSize: 20 }}>{cameraType === Camera.Constants.Type.front ? '🤳' : '📷'}</Text>
-                  </TouchableOpacity>
-                </View>
-                <View style={{ position: 'absolute', bottom: 0, flexDirection: 'row', flex: 1, width: '100%', padding: 20, justifyContent: 'space-between' }}>
-                  <View style={{ alignSelf: 'center', flex: 1, alignItems: 'center' }}>
-                    <TouchableOpacity
-                      onPress={takePicture}
-                      style={{ width: 70, height: 70, bottom: 0, borderRadius: 50, backgroundColor: '#fff' }}
-                    />
-                  </View>
-                </View>
-              </View>
-            </Camera>
-          )}
-        </View>
+      {isCameraOpen ? (
+        <Camera
+          style={styles.cameraContainer}
+          ref={cameraRef}
+          ratio="4:3"
+        >
+          <View style={styles.buttonContainer}>
+            <Button title="Take Pic" onPress={takePic} />
+          </View>
+          <StatusBar style="auto" />
+        </Camera>
       ) : (
-        <View style={{ flex: 1 }}>
-          <ImageBackground
-            source={{ uri: backgroundImage }}
-            style={{ flex: 1 }}
+        <View>
+          <Button title="Open Camera"  style={styles.container} onPress={() => setIsCameraOpen(true)} />
+          <TouchableOpacity
+            onPress={pickImage}
+            style={styles.elegir}
           >
-            <TouchableOpacity
-              onPress={pickImage}
-              style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-            >
-              <Text style={{ color: '#fff', fontWeight: 'bold', textAlign: 'center' }}>Select background image</Text>
-            </TouchableOpacity>
-          </ImageBackground>
+            <Text style={styles.botones}>Elegir imagen</Text>
+          </TouchableOpacity>
         </View>
       )}
     </View>
@@ -141,24 +96,37 @@ export default function BackgroundImage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  cameraContainer: {
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    padding: 20,
+    justifyContent: 'space-between',
+  },
+  buttonContainer: {
+    backgroundColor: '#fff',
+    alignSelf: 'flex-end',
+  },
+  preview: {
+    alignSelf: 'stretch',
+    flex: 1,
+  },
+  elegir:
+  {
+    flex: 1, 
+    justifyContent: 'center',
+     alignItems: 'center' 
+  },
+  botones:
+  {
+    color: '#fff', 
+    fontWeight: 'bold',
+     textAlign: 'center'
+  }
 });
-
-const CameraPreview = ({ photo, retakePicture }) => {
-  return (
-    <View style={{ flex: 1 }}>
-      <ImageBackground source={{ uri: photo && photo.uri }} style={{ flex: 1 }}>
-        <View style={{ flex: 1, flexDirection: 'column', padding: 15, justifyContent: 'flex-end' }}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <TouchableOpacity
-              onPress={retakePicture}
-              style={{ width: 130, height: 40, alignItems: 'center', borderRadius: 4 }}
-            >
-              <Text style={{ color: '#fff', fontSize: 20 }}>Re-take</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </ImageBackground>
-    </View>
-  );
-};
