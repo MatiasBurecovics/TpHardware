c9fdd66da9dad67bbf9b62f79b738d4b554a2820
MatiasBurecovics <111394291+MatiasBurecovics@users.noreply.github.com>
2023-11-10 00:03:07 -0300

    Add files via upload

--- BackgroundImage.js
+++ BackgroundImage.js
@@ -1,132 +1,81 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert, SafeAreaView, Button, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, ImageBackground, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { StatusBar } from 'expo-status-bar';
-import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 
-export default function BackgroundImage() {
-  let cameraRef = useRef();
-  const [hasCameraPermission, setHasCameraPermission] = useState();
-  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
-  const [photo, setPhoto] = useState();
-  const [isCameraOpen, setIsCameraOpen] = useState();
+const BackgroundImage = () => {
+  const [image, setImage] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
-      setHasCameraPermission(cameraPermission.status === "granted");
-      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
-    })();
-  }, []);
-
-  if (hasCameraPermission === undefined) {
-    return <Text>Requesting permissions...</Text>
-  } else if (!hasCameraPermission) {
-    return <Text>Permission for camera not granted. Please change this in settings.</Text>
-  }
-
-  const takePic = async () => {
-    let options = {
-      quality: 1,
-      base64: true,
-      exif: false
+  const selectImage = async () => {
+    const options = {
+      title: 'Selecciona una imagen',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
     };
 
-    let newPhoto = await cameraRef.current.takePictureAsync(options);
-    setPhoto(newPhoto);
-    setIsCameraOpen(false);
+    const response = await ImagePicker.launchImageLibraryAsync(options);
+
+    if (response.errorCode) {
+      console.log(response.errorMessage);
+    } else if (response.cancelled) {
+      console.log('Usuario canceló');
+    } else {
+      const path = response.assets[0].uri;
+      setImage(path);
+      saveImageToLibrary(path);
+    }
   };
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-  };
-
-  if (photo) {
-    let savePhoto = () => {
-      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
-        setPhoto(undefined);
-      });
+  const takePicture = async () => {
+    const options = {
+      title: 'Tomar una imagen',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+      includeBase64: true,
     };
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
-        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
-        <Button title="Discard" onPress={() => setPhoto(undefined)} />
-      </SafeAreaView>
-    );
-  }
+    const response = await ImagePicker.launchCameraAsync(options);
+
+    if (response.errorCode) {
+      console.log(response.errorMessage);
+    } else if (response.cancelled) {
+      console.log('Usuario canceló');
+    } else {
+      const uri = response.assets[0].uri;
+      setImage(uri);
+      saveImageToLibrary(uri);
+    }
+  };
+
+  const saveImageToLibrary = async (uri) => {
+    try {
+      const asset = await MediaLibrary.createAssetAsync(uri);
+      await MediaLibrary.createAlbumAsync('MyImages', asset, false);
+      console.log('Imagen guardada en la galería.');
+    } catch (error) {
+      console.log('Error al guardar la imagen: ' + error);
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      {isCameraOpen ? (
-        <Camera
-          style={styles.cameraContainer}
-          ref={cameraRef}
-          ratio="4:3"
-        >
-          <View style={styles.buttonContainer}>
-            <Button title="Take Pic" onPress={takePic} />
-          </View>
-          <StatusBar style="auto" />
-        </Camera>
-      ) : (
-        <View>
-          <Button title="Open Camera"  style={styles.container} onPress={() => setIsCameraOpen(true)} />
-          <TouchableOpacity
-            onPress={pickImage}
-            style={styles.elegir}
-          >
-            <Text style={styles.botones}>Elegir imagen</Text>
-          </TouchableOpacity>
-        </View>
-      )}
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={{ uri: image }} style={styles.imageBackground}>
+        <Button title="Seleccionar img" onPress={selectImage} />
+        <Button title="Tomar img" onPress={takePicture} />
+      </ImageBackground>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  imageBackground: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
   },
-  cameraContainer: {
-    position: 'absolute',
-    bottom: 0,
-    flexDirection: 'row',
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    padding: 20,
-    justifyContent: 'space-between',
-  },
-  buttonContainer: {
-    backgroundColor: '#fff',
-    alignSelf: 'flex-end',
-  },
-  preview: {
-    alignSelf: 'stretch',
-    flex: 1,
-  },
-  elegir:
-  {
-    flex: 1, 
-    justifyContent: 'center',
-     alignItems: 'center' 
-  },
-  botones:
-  {
-    color: '#fff', 
-    fontWeight: 'bold',
-     textAlign: 'center'
-  }
 });
+
+export default BackgroundImage;
